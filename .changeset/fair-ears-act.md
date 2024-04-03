dea390c6c03af062807772122771691fc5eed44e
Phumrapee Limpianchop <git@rayriffy.com>
2024-04-04 04:09:17 +0900

    refactor: detailed debugging

---
+++ .changeset/fair-ears-act.md
@@ -0,0 +1,5 @@
+---
+'elysia-rate-limit': patch
+---
+
+default generator throws detailed warning message with reason
--- src/services/defaultKeyGenerator.ts
+++ src/services/defaultKeyGenerator.ts
@@ -7,9 +7,21 @@ export const defaultKeyGenerator = (
   const clientAddress = server.requestIP(request)?.address
 
   if (clientAddress === undefined) {
-    console.log(
-      '[elysia-rate-limit] generator is unable to determine client IP address. Are you using older version of Bun?'
+    let reason: string
+
+    if (request === undefined)
+      reason = 'request is undefined'
+    else if (server.requestIP(request) === null)
+      reason = '.requestIP() returns null'
+    else if (server.requestIP(request)?.address === undefined)
+      reason = '.requestIP()?.address returns undefined'
+    else
+      reason = 'unknown'
+
+    console.warn(
+      `[elysia-rate-limit] failed to determine client address (reason: ${reason})`
     )
+
     return ''
   } else {
     return clientAddress
