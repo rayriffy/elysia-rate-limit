14219826d3ac26db783bec37fdbdb4b6453901c7
Phumrapee Limpianchop <git@rayriffy.com>
2024-05-24 04:32:56 +0900

    refactor: types

--- src/@types/Options.ts
+++ src/@types/Options.ts
@@ -5,39 +5,39 @@ import type { Generator } from './Generator'
 
 export interface Options {
   // The duration for plugin to remember the requests (Default: 60000ms)
-  context: Context
+  duration: number
 
   // Maximum of requests per specified duration (Default: 10)
-  countFailedRequest: boolean
+  max: number
 
   // Object to response when rate-limit reached
-  duration: number
+  errorResponse: string | Response | Error
 
   // scoping for rate limiting, set global by default to affect every request,
   // but you can adjust to local to affect only within current instance
-  errorResponse: string | Response | Error
+  scoping: 'global' | 'scoped'
 
   // should the rate limit be counted when a request result is failed (Default: false)
-  generator: Generator<any>
+  countFailedRequest: boolean
 
   // key generator function to categorize client for rate-limiting
-  headers: true
+  generator: Generator<any>
 
   // context for storing requests count
-  injectServer?: () => Server | null
+  context: Context
 
   // exposed functions for writing a custom script to skip counting i.e.,
   // not counting rate limit for some requests
   // (Default: always return false)
-  max: number
+  skip: (req: Request, key?: string) => boolean | Promise<boolean>
 
   // an explicit way to inject server instance to generator function
   // uses this as last resort only
   // since this function will slightly reduce server performance
   // (Default: not defined)
-  scoping: 'global' | 'scoped'
+  injectServer?: () => Server | null
 
-  // let plugin in control of RateLimit-* headers
+  // let the plugin in control of RateLimit-* headers
   // (Default: true)
-  skip: (req: Request, key?: string) => boolean | Promise<boolean>
+  headers: boolean
 }
