1079e8ed29808bb8f0a48b5f109b096e58150c46
Phumrapee Limpianchop <git@rayriffy.com>
2025-03-19 23:48:10 +0700

    perf: logger

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,4 +1,16 @@
 import debug from 'debug'
 
-export const logger = (unit: string, formatter: any, ...params: any[]) =>
-  debug(`elysia-rate-limit:${unit}`)(formatter, ...params)
+// create a cache of debug instances to avoid creating them on every call
+const debugCache = new Map<string, debug.Debugger>()
+
+export const logger = (unit: string, formatter: any, ...params: any[]) => {
+  const key = `elysia-rate-limit:${unit}`
+
+  let debugInstance = debugCache.get(key)
+  if (!debugInstance) {
+    debugInstance = debug(key)
+    debugCache.set(key, debugInstance)
+  }
+
+  debugInstance(formatter, ...params)
+}
